d9f14e27e3fcddba5c0189a3820cd2cb451d73aa
Rashadul Islam <120595628+rashaduldev@users.noreply.github.com>
2023-09-18 14:34:17 +0600

    Code Added

--- problem1.php
+++ problem1.php
@@ -36,4 +36,48 @@ function weatherCheck($number){
     }
     
 }
+$num=10;
+$user=isDivisibleBy5And11($num);
+echo $user;
+// if (isDivisibleBy5And11($num)) {
+//     echo"oh";
+// } else {
+//    echo"no";
+// }
+
+function isDivisibleBy5And11($num){
+    if ($num%5==0 && $num%11==0) {
+        return "true";
+    } else {
+        return "false";
+    }
+    
+}
+
+<?php
+// Function to check if a character is a vowel
+function isVowel($char) {
+    $lowercaseChar = strtolower($char);
+    if ($lowercaseChar == 'a' || $lowercaseChar == 'e' || $lowercaseChar == 'i' || $lowercaseChar == 'o' || $lowercaseChar == 'u') {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Input alphabet
+$alphabet = 'E'; // You can change this alphabet to test different cases
+
+// Check if the alphabet is a vowel
+if (ctype_alpha($alphabet) && strlen($alphabet) === 1) {
+    if (isVowel($alphabet)) {
+        echo "$alphabet is a vowel.";
+    } else {
+        echo "$alphabet is a consonant.";
+    }
+} else {
+    echo "Please enter a valid single alphabet character.";
+}
+
+
 ?>
